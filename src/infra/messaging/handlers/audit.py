c0221a0f7722d8f937c459fe9d90558b8e8f8bc6
RenataEmy <renata.emy2012@gmail.com>
2026-07-08 17:17:10 -0300

    feat: add dlq and logger

--- src/infra/messaging/handlers/audit.py
+++ src/infra/messaging/handlers/audit.py
@@ -1,11 +1,14 @@
 from src.infra.messaging.broker import broker
-from src.infra.messaging.queues import log_receiver_queue
-from src.infra.messaging.exchanges import exchange_log
+from src.infra.messaging.queues import log_receiver_queue, dlq_queue
+from src.infra.messaging.exchanges import exchange_log, dlx_exchange
 from src.infra.messaging.events.payload import AuditLogPayload
 from sqlalchemy.ext.asyncio import AsyncSession
-from faststream import Depends
+from faststream import Depends, Context
+from faststream.rabbit import RabbitMessage
 from src.core.database import get_session
 from src.models.log import AuditLog
+from src.core.logger import setup_trigger_logger
+logger = setup_trigger_logger()
 
 @broker.subscriber(
     queue=log_receiver_queue,
@@ -13,12 +16,45 @@ from src.models.log import AuditLog
 )
 async def saved_log_audit(
     data: AuditLogPayload,
+    msg: RabbitMessage,
     session: AsyncSession = Depends(get_session)
 ):
     try:
         new_log = AuditLog(
             event_id=data.log_id,
-            user_id=data.actor,
+            user_id=data.actor.user_id,
+            entity_type=data.resource,
+            entity_id=str(data.resource_id),
+            service_source=data.microservice,
+            action=data.action,
+            delta=data.changes,
+            reason=data.reason,
+            event_timestamp=data.timestamp
+        )
+    
+        session.add(new_log)
+        await session.commit()
+        logger.info(f"Log do serviço {new_log.service_source} salvo no banco com sucesso")
+        await msg.ack()
+
+    except Exception as e:
+        await session.rollback()
+        logger.error(f"Falha ao gravar AuditLog no banco: {str(e)}", exc_info=True)
+        await msg.reject()
+
+@broker.subscriber(
+    queue=dlq_queue,
+    exchange=dlx_exchange
+)
+async def reprocess_dlq_audit(
+    data: AuditLogPayload,
+    msg: RabbitMessage,
+    session: AsyncSession = Depends(get_session)
+):
+    try:
+        new_log = AuditLog(
+            event_id=data.log_id,
+            user_id=data.actor.user_id,
             entity_type=data.resource,
             entity_id=str(data.resource_id),
             service_source=data.microservice,
@@ -31,6 +67,10 @@ async def saved_log_audit(
         session.add(new_log)
 
         await session.commit()
+        logger.info(f"Log do serviço {new_log.service_source} salvo no banco com sucesso")
+        await msg.ack()
 
     except Exception as e:
         await session.rollback()
+        logger.error(f"Falha ao gravar AuditLog no banco: {str(e)}", exc_info=True)
+        await msg.nack(requeue=True)
--- src/infra/messaging/queues.py
+++ src/infra/messaging/queues.py
@@ -1,8 +1,18 @@
 from faststream.rabbit import RabbitQueue
-from .constants import RoutingKey, Queue
+from .constants import RoutingKey, Queue, Exchange
 
 log_receiver_queue = RabbitQueue(
     name=Queue.LOG_REGISTER_QUEUE,
     routing_key=RoutingKey.LOG_REGISTER_ROUTING_KEY,
+    durable=True,
+    arguments={
+        "x-dead-letter-exchange": Exchange.DLX_EXCHANGE.value,
+        "x-dead-letter-routing-key": RoutingKey.LOG_REGISTER_DLQ_ROUTING_KEY.value
+    }
+)
+
+dlq_queue = RabbitQueue(
+    name=Queue.LOG_REGISTER_DLQ,
+    routing_key=RoutingKey.LOG_REGISTER_DLQ_ROUTING_KEY,
     durable=True
 )
